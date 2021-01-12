dc8f361315a7d3d45d67cf98482bc193d7be1501
Joseph Rhines <jprhines@gmail.com>
2021-01-11 21:05:35 -0600

    I added the moment date and time and the event listener for the save buttons

---
+++ logic.js
@@ -0,0 +1,11 @@
+//  var allButtons =document.getElementsByClassName("button");
+
+moment(Date);
+$("#currentDay").text(moment().format('dddd MMMM Do YYYY, h:mm a'));
+
+
+
+
+$(".saveBtn").on("click", function(){
+    console.log("on")
+  });
