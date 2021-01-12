75653644e0ec51c21bc6b1645b788b32f8797768
Joseph Rhines <jprhines@gmail.com>
2021-01-12 17:14:37 -0600

    I added localstorage to the input fields that are connected to the save buttons. I also added a for loop so each button doesn't overwrite the text in the other inputs.

--- logic.js
+++ logic.js
@@ -3,9 +3,13 @@
 moment(Date);
 $("#currentDay").text(moment().format('dddd MMMM Do YYYY, h:mm a'));
 
-
-
+for (let i = 1; i <= 9; i++) {
+    $("#t" + i).val(localStorage.getItem(i))
+}
 
 $(".saveBtn").on("click", function(){
-    console.log("on")
+    console.log($("#t" +this.id).val())
+    
+    // $(this).val(testarea)
+    localStorage.setItem(this.id, $("#t" +this.id).val() )
   });
